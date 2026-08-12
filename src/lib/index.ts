ff1b278af539f6ac9a91826746396f12829a9205
james-sidiqi <amir.sidiqi@me.com>
2026-08-12 13:16:45 +0430

    Export memoized site data helpers

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
 export { buildSiteData } from './data/buildRelations.js';
+export { getSiteData, resetSiteDataCache } from './data/site-data.js';
 export { siteConfig } from './config.js';
 export type { SiteData, Tour, Attraction, Province, Region, Location, Dish, Faq, HotelProperty, HotelRoom, ItineraryDay, InclusionDay, TourDate } from './types/view-models.js';
